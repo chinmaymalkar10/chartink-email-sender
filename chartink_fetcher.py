6b22e91dba623eb10acd8bfa5f4c20fc2b1c60e2
Chinmay Malkar <63865999+chinmaymalkar10@users.noreply.github.com>
2025-06-20 13:11:53 +0530

    Update chartink_fetcher.py

--- chartink_fetcher.py
+++ chartink_fetcher.py
@@ -6,8 +6,15 @@ from email_sender import send_email
 
 charting_link = "https://chartink.com/screener/"
 charting_url = "https://chartink.com/screener/process"
+stocks = [
+    "ACC", "AFFLE", "ASTRAL", "AUROPHARMA", "BAJAJFINSV", "BANKBARODA", "BDL", "BHARTIARTL", "CDSL",
+    "CHOLAFIN", "CIPLA", "DRREDDY", "GLENMARK", "MEDANTA", "HCLTECH", "HDFCBANK", "HAL", "HINDUNILVR",
+    "ICICIGI", "INDUSINDBK", "INFY", "LT", "LUPIN", "LODHA", "MAXHEALTH", "NESTLEIND", "PATANJALI",
+    "POLICYBZR", "PIDILITIND", "PVRINOX", "RADICO", "RELIANCE", "SBICARD", "SRF", "SBIN", "SUNPHARMA",
+    "TATACHEM", "TCS", "TATACONSUM", "TECHM", "TITAN", "TORNTPHARM", "TORNTPOWER", "UBL", "UNITDSPR", "VOLTAS"
+]
 
-condition = "( {57960} ( [0] 15 minute close > [-1] 15 minute max ( 20 , [0] 15 minute close ) and [0] 15 minute volume > [0] 15 minute sma ( volume,20 ) ) ) "
+condition = "( ( [0] 5 minute adx ( 14 ) > 25 and [0] 5 minute close >= 800 and [0] 5 minute close <= 4000 and [0] 5 minute close < [0] 5 minute sma ( close,8 ) and [0] 5 minute close < 1 day ago low and [-1] 5 minute close > 1 day ago low and abs ( 1 day ago low - latest open ) <= latest open * 0.007 ) ) "
 
 def getData(payload):
     payload = {'scan_clause': payload}
@@ -21,7 +28,8 @@ def getData(payload):
 
             df = []
             for item in r.json()['data']:
-                df.append(item['name'])
+                if item['nsecode'] in stocks:
+                    df.append(item['name'])
             return df
     except Exception as e:
         print(f"Chartink fetch error: {e}")
