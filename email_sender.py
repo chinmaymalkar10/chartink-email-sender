c8517dbe38b777fb631da75b6c10289ad6ef1697
Chinmay Malkar <63865999+chinmaymalkar10@users.noreply.github.com>
2025-06-23 15:45:32 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -2,25 +2,51 @@ import smtplib
 from email.message import EmailMessage
 from datetime import datetime
 import pytz
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
 
-EMAIL_SENDER = 'onliogs69@gmail.com'
-EMAIL_PASSWORD = 'jfvp avby mhwj trlx'  # Use Gmail App Password
-EMAIL_RECEIVER = 'chinmaymalkar10@gmail.com'
+# Load credentials from .env
+load_dotenv()
 
-def send_email(stock_name):
+EMAIL_SENDER = os.getenv('EMAIL_SENDER')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+TWILIO_WHATSAPP_FROM = os.getenv('TWILIO_WHATSAPP_FROM')
+WHATSAPP_TO = os.getenv('WHATSAPP_TO')
+
+def send_notification(stock_name, signal_type):
+    send_email(stock_name, signal_type)
+    send_whatsapp(stock_name, signal_type)
+
+def send_email(stock_name, signal_type):
     msg = EmailMessage()
-    msg['Subject'] = "Sell Entry"
+    msg['Subject'] = f"{signal_type} Alert"
     msg['From'] = EMAIL_SENDER
     msg['To'] = EMAIL_RECEIVER
-    msg.set_content(f"Sell signal for: {stock_name}")
+    body = f"{signal_type} detected for stock: {stock_name}"
+    msg.set_content(body)
 
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
             smtp.send_message(msg)
-        log(f"Email sent for: {stock_name}")
+        log(f"{signal_type} | {stock_name}")
     except Exception as e:
         log(f"Error sending email: {e}")
+        
+def send_whatsapp(stock_name, signal_type):
+    try:
+        body = f"{signal_type} detected for stock: {stock_name}"
+        message = client.messages.create(
+                  from_=f'whatsapp:{TWILIO_WHATSAPP_FROM}',
+                  body=body,
+                  to=f'whatsapp:{WHATSAPP_TO}')
+    except Exception as e:
+        log(f"WhatsApp error for {signal_type} - {stock_name}: {e}")
 
 def log(message):
     ist = pytz.timezone("Asia/Kolkata")
