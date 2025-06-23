f08fe7e7d89cfd94930e6fc83a844904073eda00
Chinmay Malkar <63865999+chinmaymalkar10@users.noreply.github.com>
2025-06-23 15:49:00 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,15 +2,25 @@ function fetchLogs() {
     fetch('/logs')
         .then(res => res.json())
         .then(data => {
-            const logBox = document.getElementById("logBox");
-            logBox.innerHTML = '';
-            data.forEach(line => {
+            const buyDiv = document.getElementById("buyLogs");
+            const sellDiv = document.getElementById("sellLogs");
+
+            buyDiv.innerHTML = "";
+            sellDiv.innerHTML = "";
+
+            data.buy.forEach(line => {
                 const p = document.createElement('p');
-                p.textContent = line.trim();
-                logBox.appendChild(p);
+                p.textContent = line;
+                buyDiv.appendChild(p);
+            });
+
+            data.sell.forEach(line => {
+                const p = document.createElement('p');
+                p.textContent = line;
+                sellDiv.appendChild(p);
             });
         });
 }
 
-setInterval(fetchLogs, 5000); // every 5 sec
+setInterval(fetchLogs, 5000);
 window.onload = fetchLogs;
